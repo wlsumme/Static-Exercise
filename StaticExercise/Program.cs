3910f7adb1c0f34e37b7958ace125e7fa45f7aef
William Summe <wlsumme@gmail.com>
2024-05-20 14:06:44 -0500

    done

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var fah = TempConverter.CelsiusToFahrenhiet(17);
+
+            var cel = TempConverter.FahrenheitToCelsius(62.8);
+
+            Console.WriteLine($"17 degrees celsius is {fah} degees fahrenhiet.");
+            Console.WriteLine($"62.8 degrees fahrenhiet is {cel} degrees celsius.");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double f)
+        {
+            var c = (f - 32) / 1.8;
+            return c;
+
+        }
+
+        public static double CelsiusToFahrenhiet(double c) 
+        {
+            return (c * 9) / 5 + 32;
+        }
+
+            
+    }
+}
